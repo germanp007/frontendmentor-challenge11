9061be42544fb303e2efc766216cd94106092e67
germanp007 <pintogerman281@gmail.com>
2023-09-01 20:41:17 -0300

    Quinto commit

--- src/Card.tsx
+++ src/Card.tsx
@@ -12,19 +12,21 @@ interface DataType {
   col: string;
 }
 
-const Card: React.FC<DataType> = ({ title, description, color, icon, col }) => {
+const Card: React.FC<DataType> = ({ ...props }) => {
   return (
     <article
-      className={`shadow-2xl w-[314px] h-[224px] bg-white m-auto px-[30px] pt-[35px] pb-[25px] flex flex-col justify-between rounded-lg border-t-[5px] ${color} md:${col} `}
+      className={`shadow-2xl w-[314px] h-[224px] bg-white m-auto px-[30px] pt-[35px] pb-[25px] flex flex-col justify-between rounded-lg border-t-[5px] ${props.color} md:${props.col} `}
     >
       <div>
         <h1 className="text-left text-[20px] text-Very-Dark-Blue font-[800]">
-          {title}
+          {props.title}
         </h1>
-        <p className="text-left text-[13px] text-Grayish-Blue">{description}</p>
+        <p className="text-left text-[13px] text-Grayish-Blue">
+          {props.description}
+        </p>
       </div>
       <div className="flex justify-end">
-        <img src={icon} alt="lupa" className="h-[58px] w-[58px] " />
+        <img src={props.icon} alt="lupa" className="h-[58px] w-[58px] " />
       </div>
     </article>
   );
